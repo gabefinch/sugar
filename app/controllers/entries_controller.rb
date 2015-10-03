7090e5378a0721ba1f36b631cb4997045d8fb30d
Gabe Finch <gabefinch@gmail.com>
2015-10-03 15:34:10 -0700

    Add new,create,report to entries controller

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,7 +1,21 @@
 class EntriesController < ApplicationController
 
   def index
-    
   end
+  def new
+    @entry = Entry.new
+  end
+  def create
+    @entry = entry.new(entry_params)
+    if @entry.save
+      flash[:notice] = "Entry added!"
+      redirect_to root
+    else
+      flash[:error] = "There was a problem with the entry. Please try again."
+      render :new
+    end
+  end
+  def reports
 
+  end
 end
